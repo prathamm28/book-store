7b2a200a496074bb650e337df9e380a7e7dce828
prathamm28 <165469966+prathamm28@users.noreply.github.com>
2024-08-17 16:09:54 +0530

    latest

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import Navbar from './assets/components/Navbar'
+import Navbar from './assets/components/Navbar';
+import Banner from './assets/components/Banner';
+import Footer from './assets/components/Footer';
+import Freebook from './assets/components/Freebook';
 
 const App = () => {
   return (
     < >
       <Navbar/>
+      <Banner/>
+      <Freebook/>
+      <Footer/>
     </>
   )
 }
--- Frontend/src/assets/components/Navbar.jsx
+++ Frontend/src/assets/components/Navbar.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false)
+  useEffect(()=>{
+      const handleScroll = () => {
+        if(window.scrollY > 0) {
+          setSticky(true)
+        }
+        else{
+          setSticky(false)
+        }
+      }
+      window.addEventListener('scroll', handleScroll)
+      return ()=>{
+        window.removeEventListener('scroll', handleScroll)
+      }
+  },[]);
     const navItems=(
     <>
     <li>
@@ -15,11 +30,16 @@ const Navbar = () => {
         <li>
         <a>About</a>
         </li>
-    </>)
+    </>
+    )
   return (
     <>
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-    <div className="navbar bg-base-100">
+    <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 z-50${
+      sticky
+      ?"sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out"
+      : ""
+    }`}>
+    <div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -49,8 +69,8 @@ const Navbar = () => {
     <ul className="menu menu-horizontal px-1">{navItems}</ul>
   </div>
   <div className='hidden md:block'>
-  <label className="input input-bordered flex items-center gap-2">
-  <input type="text" className="grow" placeholder="Search" />
+  <label className="px-3 py-1 border rounded-md flex items-center gap-2">
+  <input type="text" className="grow outline-none" placeholder="Search" />
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
@@ -69,7 +89,7 @@ const Navbar = () => {
 
   {/* sun icon */}
   <svg
-    className="swap-off h-8 w-8 fill-current"
+    className="swap-off h-7 w-7 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24">
     <path
@@ -78,7 +98,7 @@ const Navbar = () => {
 
   {/* moon icon */}
   <svg
-    className="swap-on h-8 w-8 fill-current"
+    className="swap-on h-7 w-7 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24">
     <path
